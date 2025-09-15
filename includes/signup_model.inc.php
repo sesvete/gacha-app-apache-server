92b2e4c49ab2ab784f9b306d9d5d74ee0df6ab63
Simon Svetec <simon.svetec@gmail.com>
2025-09-16 00:00:03 +0200

    add insert into counter table

--- includes/signup_model.inc.php
+++ includes/signup_model.inc.php
@@ -32,3 +32,35 @@ function set_user(object $pdo, string $username, string $password)
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
     return $result;
 }
+
+function create_user_counter(object $pdo, int $uid){
+    $combinations = [
+        ['game' => 'genshin_impact', 'banner' => 'chronicled_wish'],
+        ['game' => 'genshin_impact', 'banner' => 'limited'],
+        ['game' => 'genshin_impact', 'banner' => 'standard'],
+        ['game' => 'genshin_impact', 'banner' => 'weapon'],
+        ['game' => 'honkai_star_rail', 'banner' => 'collaboration_character'],
+        ['game' => 'honkai_star_rail', 'banner' => 'collaboration_light_cone'],
+        ['game' => 'honkai_star_rail', 'banner' => 'light_cone'],
+        ['game' => 'honkai_star_rail', 'banner' => 'limited'],
+        ['game' => 'honkai_star_rail', 'banner' => 'standard'],
+        ['game' => 'zenless_zone_zero', 'banner' => 'bangboo'],
+        ['game' => 'zenless_zone_zero', 'banner' => 'limited'],
+        ['game' => 'zenless_zone_zero', 'banner' => 'standard'],
+        ['game' => 'zenless_zone_zero', 'banner' => 'w_engine'],
+        ['game' => 'tribe_nine', 'banner' => 'limited'],
+        ['game' => 'tribe_nine', 'banner' => 'standard'],
+        ['game' => 'tribe_nine', 'banner' => 'tension_card']
+    ];
+
+    $query = "INSERT INTO counter (user_uid, game, banner) VALUES (:uid, :game, :banner);";
+    $stmt = $pdo->prepare($query);
+
+    // Loop through the combinations and execute the prepared statement
+    foreach ($combinations as $combo) {
+        $stmt->bindParam(":uid", $uid);
+        $stmt->bindParam(":game", $combo['game']);
+        $stmt->bindParam(":banner", $combo['banner']);
+        $stmt->execute();
+    }
+}
--- signup.php
+++ signup.php
@@ -9,6 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         require_once 'includes/signup_model.inc.php';
         require_once 'includes/signup_contr.inc.php';
 
+
         if (is_input_empty($username, $password)){
             http_response_code(400); // Bad Request
             echo json_encode(["status" => "error", "message" => "Please fill all forms."]);
@@ -18,15 +19,25 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             echo json_encode(["status" => "error", "message" => "Username already taken."]);
             die();
         } else {
+            // turns of autocommit mode
+            $pdo->beginTransaction();
+
             create_user($pdo, $username, $password);
-            // tu se bodo tudi naredili zapisi v counter tabeli
+            $uid = $pdo->lastInsertId();
+            create_user_counter($pdo, $uid);
+
+            // commit database transcation
+            $pdo->commit();
 
             http_response_code(201); // Created
             echo json_encode(["status" => "success", "message" => "Account created successfully."]);
             error_log("signup: successfully created user!");
         }
     } catch (PDOException $e){
-        die("Query failed: " . $e->getMessage());
+        $pdo->rollBack();
+        http_response_code(500);
+        echo json_encode(["status" => "error", "message" => "Database operation failed: " . $e->getMessage()]);
+        die();
     }
 } else {
     error_log("Sigup: not a post connection");
