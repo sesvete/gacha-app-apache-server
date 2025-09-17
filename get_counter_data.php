0b80fa1547a0c87feda7fdc735ef0d831b59508f
Simon Svetec <simon.svetec@gmail.com>
2025-09-18 00:36:47 +0200

    add retrieval of newest unit

--- get_counter_data.php
+++ get_counter_data.php
@@ -12,21 +12,44 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         require_once 'includes/dbh.inc.php';
         require_once 'includes/get_counter_data_model.inc.php';
 
-        $result = get_counter_data_form_db($pdo, $uid, $game, $banner);
-        if ($result) {
-            http_response_code(200);
-            echo json_encode([
-                "progress" => $result["progress"],
-                "guaranteed" => $result["guaranteed"]
-            ]);
+        // turns of autocommit mode
+        $pdo->beginTransaction();
+
+        $result_counter = get_counter_data_form_db($pdo, $uid, $game, $banner);
+
+        // also get data from last pulled unit
+        if ($result_counter) {
+            $result_pull = get_last_pulled_unit($pdo, $uid, $game, $banner);
+
+            // commit database transcation
+            $pdo->commit();
+            if ($result_pull) {
+                echo json_encode([
+                    "progress" => $result_counter["progress"],
+                    "guaranteed" => $result_counter["guaranteed"],
+                    "numOfPulls" => $result_pull["num_of_pulls"],
+                    "unitName" => $result_pull["unit_name"],
+                    "fromBanner" => $result_pull["from_banner"]
+                ]);
+            } else {
+                echo json_encode([
+                    "progress" => $result_counter["progress"],
+                    "guaranteed" => $result_counter["guaranteed"],
+                    "numOfPulls" => null,
+                    "unitName" => null,
+                    "fromBanner" => null
+                ]);
+            }
 
         } else {
+            $pdo->rollBack();
             http_response_code(500);
             echo json_encode(["status" => "error", "message" => "Database operation failed: " . $e->getMessage()]);
             die();
         }
         
     } catch (PDOException $e) {
+        $pdo->rollBack();
         http_response_code(500);
         echo json_encode(["status" => "error", "message" => "Database operation failed: " . $e->getMessage()]);
         die();
--- includes/get_counter_data_model.inc.php
+++ includes/get_counter_data_model.inc.php
@@ -14,3 +14,20 @@ function get_counter_data_form_db(object $pdo, int $uid, string $game, string $b
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
     return $result;
 }
+
+function get_last_pulled_unit(object $pdo, int $uid, string $game, string $banner){
+    $query = "SELECT unit_name, num_of_pulls, from_banner FROM pull
+                WHERE(user_uid = :uid AND game = :game AND banner = :banner)
+                ORDER BY pull_id DESC
+                LIMIT 1;";
+    
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(":uid", $uid);
+    $stmt->bindParam(":game", $game);
+    $stmt->bindParam(":banner", $banner);
+
+    $stmt->execute();
+
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $result;
+}
